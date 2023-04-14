d3bc83670999913d2e7bc6bd9c398c57a5c8e3c3
southoftheriver <anwjsrlrhwkd@naver.com>
2023-04-14 19:27:47 +0900

    project setting

---
+++ src/main/java/jy/demo/NeverEndGrooveApplication.java
@@ -0,0 +1,13 @@
+package jy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverEndGrooveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverEndGrooveApplication.class, args);
+	}
+
+}
