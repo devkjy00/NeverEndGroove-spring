3dd1c2e18b7d940b4b858b61a6dfe5dfbada971d
southoftheriver <anwjsrlrhwkd@naver.com>
2023-04-26 23:43:32 +0900

    feat: caching access token

--- src/main/java/jy/demo/service/UserService.java
+++ src/main/java/jy/demo/service/UserService.java
@@ -20,12 +20,12 @@ public class UserService {
 
 
     @Transactional
-    public void saveOrUpdate(CustomOAuth2User customOAuth2User) {
+    public User saveOrUpdate(CustomOAuth2User customOAuth2User) {
         User user = userRepository.findBySocialProviderKey(customOAuth2User.getSocialProviderKey())
             .map(savedUser -> savedUser.update(customOAuth2User))
             .orElse(customOAuth2User.toUser());
 
-        userRepository.save(user);
+        return userRepository.save(user);
     }
 
 }
